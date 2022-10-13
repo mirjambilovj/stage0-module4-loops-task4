14f5076def9709a3aedc013cccb60876ec82d417
mirjambilovj <106926296+mirjambilovj@users.noreply.github.com>
2022-10-13 20:36:03 +0500

    numbertobreak updated

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-
+        int sum1 = 0;
+        int sum2 = 0;
+        if (0 > lastInRow) {
+            System.out.println("last number in row is negative");
+        } else if (numberToSkip > lastInRow) {
+            System.out.println("number to skip is bugger then the last");
+        } else {
+            for (int i = 0; i <= numberToSkip; i++) {
+                sum1 = sum1 + i;
+            }
+            for (int l = 0; l <= lastInRow; l++) {
+                sum2 = sum2 + l;
+            }
+            System.out.println("skipped sum is " + sum1 + "\ncounted sum is " + (sum2 - sum1));
+        }
     }
 }
